f0ac6492c4a745686f9f4ba0264ec1e263963ec1
Maks <mprihodko92@gmail.com>
2016-09-01 01:11:18 +0300

    add tpl  and fonts

--- baccardi/header.php
+++ baccardi/header.php
@@ -10,17 +10,56 @@
  */
 
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<?php endif; ?>
-	<?php wp_head(); ?>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?> - <?php bloginfo('description'); ?>">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-	<div id="page" class="site"><!-- .site-content -->
- 
+<header id="bc_header">
+    
+    <!-- ******************* The Navbar Area ******************* -->
+    <div class="nav-top">
+        <div class="container">
+            <nav class="navbar ">
+			  	<div class="container-fluid">
+			    <!-- Brand and toggle get grouped for better mobile display -->
+			    	<div class="navbar-header">
+				      	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#primary-menu" aria-expanded="false">
+					        <span class="sr-only">Toggle navigation</span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+				      	</button>
+				     	<a class="navbar-brand logo" style="background-image: url(<?=get_template_directory_uri()?>/bacardi-logo.png)" href="<?=home_url()?>">
+				     	 
+				     	</a>
+				    </div>
+
+				    <!-- Collect the nav links, forms, and other content for toggling -->
+				    <div class="collapse navbar-collapse" id="primary-menu">
+				      <?php wp_nav_menu(
+			                            array(
+			                                'theme_location' => 'primary',
+			                                'menu_class' => 'cat-menu nav navbar-nav',
+			                                'fallback_cb' => '',
+			                                'desc_depth' => 1,				                                
+			                                // 'walker' => new fastpaleo_walker()
+			                            )
+				                       ); ?>
+				    </div><!-- /.navbar-collapse -->
+			  	</div><!-- /.container-fluid -->
+			</nav>
+                       
+        </div>
+    </div>
+   
+</header>
